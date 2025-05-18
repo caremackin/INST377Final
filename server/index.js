fbc9ef22ea4d723e42815ba2427fd0bfcb2b3d3d
caremackin <cmackin712@gmail.com>
2025-05-18 18:15:16 -0400

    deployment stuff

--- server/index.js
+++ server/index.js
@@ -11,7 +11,4 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello, Express.js Server!</h1>");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = app;
